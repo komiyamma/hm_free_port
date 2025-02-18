ec5b7675390922f205c62878e467c950ca0374ac
komiyamma <komiyama0629@gmail.com>
2025-02-18 20:52:08 +0900

    ...

---
+++ HmAvailablePort/HmAvailablePort/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Net.NetworkInformation;
+
+namespace HmAvailablePort
+{
+    internal class Program
+    {
+        static List<int> portsInUse;
+
+        public static int AvailablePort()
+        {
+            var ipGP = IPGlobalProperties.GetIPGlobalProperties();
+            var tcpEPs = ipGP.GetActiveTcpListeners();
+            var udpEPs = ipGP.GetActiveUdpListeners();
+            portsInUse = tcpEPs.Concat(udpEPs).Select(p => p.Port).ToList();
+
+            for (int port = 49152; port <= 65535; port++)
+            {
+                if (!portsInUse.Contains(port))
+                {
+                    return port;
+                }
+            }
+
+            return 0; // 空きポートが見つからない場合
+        }
+
+
+        static int Main(string[] args)
+        {
+            int port = AvailablePort();
+            Console.WriteLine("PORT:" + port);
+            if (port > 0) { return 0; } else { return 1; }
+        }
+    }
+}
---
+++ HmAvailablePort/HmAvailablePort/bin/Release/HmAvailablePort.js
@@ -0,0 +1,31 @@
+﻿if (typeof(findAvailablePortProcessInfo) != "undefined") {
+    findAvailablePortProcessInfo.kill();
+}
+
+function findAvailablePortAndExecFunc(callBackFunc) {
+
+    findAvailablePortProcessInfo = hidemaru.runProcess(findAvailablePortAndExecFunc.currentMacroDirectory + "\\HmAvailablePort.exe", ".", "stdio", "utf8");
+    var stdOut = findAvailablePortProcessInfo.stdOut;
+    stdOut.onReadAll(readAllAsync);
+
+    function readAllAsync(outline) {
+        try {
+	        var match = /PORT:(\d+)/.exec(outline);
+	        if (match) {
+	            var port = parseInt(match[1], 10);
+	            if (callBackFunc) {
+	                callBackFunc(port);
+	            }
+	        } else {
+	            console.log("no match");
+	        }
+        } catch(e) {
+        } finally {
+            if (findAvailablePortProcessInfo) {
+                findAvailablePortProcessInfo.kill();
+            }
+        }
+    }
+}
+
+findAvailablePortAndExecFunc.currentMacroDirectory = currentmacrodirectory();
