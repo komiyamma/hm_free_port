024ee802f4325264676c3f7c847fc2c980c9ea33
komiyamma <komiyama0629@gmail.com>
2025-02-24 01:00:04 +0900

    リポジトリ不具合により、コミットしなおし。

---
+++ HmFreePort/HmFreePort/Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+
+namespace HmFreePort
+{
+    [ComVisible(true)]
+    [ClassInterface(ClassInterfaceType.None)] // これは必須
+    [Guid("B4280D3B-413D-4630-88A5-7AA68B562226")] // GUIDはそれぞれのclassで生成しなおすこと。
+
+    public class HmFreePort
+    {
+
+        public int Port
+        {
+            get
+            {
+                return GetFreePort();
+            }
+        }
+
+        static private int GetFreePort()
+        {
+            try
+            {
+                var ipGP = IPGlobalProperties.GetIPGlobalProperties();
+                if (ipGP == null)
+                {
+                    // Console.WriteLine("IPGlobalPropertiesの取得に失敗しました。");
+                    return 0;
+                }
+
+                var usedPorts = new HashSet<int>(ipGP.GetActiveTcpListeners()
+                                                  .Concat(ipGP.GetActiveUdpListeners())
+                                                  .Select(endpoint => endpoint.Port));
+
+                for (int port = 49152; port <= 65535; port++)
+                {
+                    if (!usedPorts.Contains(port))
+                    {
+                        // Console.WriteLine($"利用可能なポートが見つかりました: {port}");
+                        return port;
+                    }
+                }
+
+                // Console.WriteLine("利用可能なポートが見つかりませんでした。");
+                return 0;
+            }
+            catch (NetworkInformationException ex)
+            {
+                // Console.WriteLine("ネットワーク接続中エラー");
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                // Console.WriteLine("なんか知らんがエラー");
+                return 0;
+            }
+
+        }
+
+        static int Main(string[] args)
+        {
+            HmFreePort ap = new HmFreePort();
+            int port = ap.Port;
+            Console.WriteLine(port);
+            if (port > 0) { return 0; } else { return 1; }
+        }
+    }
+}
---
+++ HmFreePort/HmFreePortNative/dllmain.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <vector>
+#include <winsock2.h>
+#include <iphlpapi.h>
+
+#pragma comment(lib, "iphlpapi.lib")
+#pragma comment(lib, "ws2_32.lib")
+
+std::vector<int> GetUsedPorts() {
+    std::vector<int> usedPorts;
+    ULONG outBufLen = sizeof(MIB_TCPTABLE2);
+    PMIB_TCPTABLE2 pTcpTable = (MIB_TCPTABLE2*)malloc(outBufLen);
+
+    if (GetExtendedTcpTable(pTcpTable, &outBufLen, FALSE, AF_INET, TCP_TABLE_OWNER_PID_ALL, 0) == NO_ERROR) {
+        for (unsigned int i = 0; i < pTcpTable->dwNumEntries; ++i) {
+            usedPorts.push_back(ntohs(pTcpTable->table[i].dwLocalPort));
+        }
+    }
+
+    free(pTcpTable);
+
+    outBufLen = sizeof(MIB_UDPTABLE);
+    PMIB_UDPTABLE pUdpTable = (MIB_UDPTABLE*)malloc(outBufLen);
+
+    if (GetExtendedUdpTable(pUdpTable, &outBufLen, FALSE, AF_INET, UDP_TABLE_OWNER_PID, 0) == NO_ERROR) {
+        for (unsigned int i = 0; i < pUdpTable->dwNumEntries; ++i) {
+            usedPorts.push_back(ntohs(pUdpTable->table[i].dwLocalPort));
+        }
+    }
+
+    free(pUdpTable);
+
+    return usedPorts;
+}
+
+extern "C" __declspec(dllexport) intptr_t GetFreePort() {
+    std::vector<int> portsInUse = GetUsedPorts();
+
+    for (int port = 49152; port <= 65535; ++port) {
+        if (std::find(portsInUse.begin(), portsInUse.end(), port) == portsInUse.end()) {
+            return port;
+        }
+    }
+
+    return 0; // 空きポートが見つからない場合
+}
